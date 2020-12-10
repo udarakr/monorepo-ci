1218d56a13a8e073743f9326e372fd5bda91b38e
Udara Rathnayake <udarakr@gmail.com>
2020-12-10 13:35:27 +0530

    fix path

--- monorepo-ci-space/lighthouserc.js
+++ monorepo-ci-space/lighthouserc.js
@@ -2,9 +2,15 @@ module.exports = {
     ci: {
       collect: {
         /* Add configuration here */
-        startServerCommand: 'ng serve',
+        startServerCommand: 'cd monorepo-ci-space && ng serve',
         url: ['http://localhost:4200']
       },
+    //   assert: {
+    //     assertions: {
+    //       'categories:performance': ['warn', {minScore: 1}],
+    //       'categories:accessibility': ['error', {minScore: 1}]
+    //     }
+    //   },
       upload: {
         /* Add configuration here */
         target: 'temporary-public-storage'
